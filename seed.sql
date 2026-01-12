c31b2244b71fcfee2475fe39c4b202eea97c094c
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2026-01-12 11:19:37 -0300

    Feat: adiciona seed ao banco de dados

--- seed.sql
+++ seed.sql
@@ -1,1 +1,11 @@
--- seed
+INSERT INTO books (name, category, price) VALUES
+('Harry Potter', 'Fantasia', 39.90),
+('O Hobbit', 'Fantasia', 29.90),
+('Clean Code', 'Programação', 79.90),
+('Arquitetura Limpa', 'Programação', 89.90),
+('1984', 'Ficção', 24.90),
+('Dom Quixote', 'Clássico', 34.90),
+('It', 'Terror', 49.90),
+('O Iluminado', 'Terror', 44.90),
+('Sapiens', 'História', 59.90),
+('Homo Deus', 'História', 64.90);
